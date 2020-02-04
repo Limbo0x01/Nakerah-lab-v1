6d493cb052c9fc4cefbd75c3b43b0143c20e6a24
Lembo <41475273+0xL1mb0@users.noreply.github.com>
2020-02-04 20:47:24 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
+
 #!/bin/bash
-php_v=$(php --version | grep -i 'PHP'|cut -d ')' -f 1 | head -1 | cut -c 5-10|cut -d '.' -f 1,2)
-export php_v
+sudo apt-get install php7.2-curl php7.2-mbstring php7.2-xml -y
+sudo apt-get install php7.3-curl php7.3-mbstring php7.3-xml -y
+sudo apt-get install php php-mysqli php-gd libapache2-mod-php -y
+sudo apt install hhvm
 
-sudo source ~/.bashrc
+export PHP_limbo=$(php --version | grep -i 'PHP'|cut -d ')' -f 1 | head -1 | cut -c 5-10|cut -d '.' -f 1,2)
 
-sudo python Nakerah-lab.py
+
+source ~/.bashrc
+echo $PHP_limbo
+
+sudo python Nakerah-lab.py $PHP_limbo
