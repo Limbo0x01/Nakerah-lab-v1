3cfbbb1d1ea4378a57a0e5b0969b61d5d6d08597
Lembo <41475273+0xL1mb0@users.noreply.github.com>
2020-02-04 21:38:01 +0200

    Update Nakerah-lab.py

--- Nakerah-lab.py
+++ Nakerah-lab.py
@@ -1,60 +1,57 @@
 #!/usr/bin/python
 import os
 import time
-import sys
-
 f = open('banner.txt', 'r')
 file_contents = f.read()
-print(file_contents)
+print("file_contents")
 f.close()
-PHP_V=sys.argv[1]
-
+PHP_V=os.environ['php_v']
 hostname = 'localhost'
 username = 'root'
 password = ''
 database = 'mysql'
 
-print "Step 1: Install apache"
+print("Step 1: Install apache")
 os.system("sudo apt-get install unzip")
-#print "[+] Update your local package : "
+#print("[+] Update your local package : ")
 #os.system("sudo apt update")
-print "[+] Install the apache2 package:"
+print("[+] Install the apache2 package:")
 os.system("sudo apt-get install apache2 -y")
-print "[+] Enable its rewrite module, and restart the service :"
+print("[+] Enable its rewrite module, and restart the service :")
 os.system("sudo a2enmod rewrite && systemctl restart apache2")
-print "[+] Step 2 - Adjusting the Firewall "
+print("[+] Step 2 - Adjusting the Firewall ")
 os.system("sudo ufw app list")
 os.system("sudo ufw allow 'Apache'")
 os.system("sudo ufw status'")
 #-----------------------------------------------------------#
-print "-------------------------------------------------------"
-print "[+] Step 3 - Checking your Web Server"
+print("-------------------------------------------------------")
+print("[+] Step 3 - Checking your Web Server")
 os.system("sudo systemctl status apache2 --no-pager")
-print "[+] Step 4 - Setting Up Virtual Hosts (Recommended)"
-print "[+] Assign ownership of the directory:"
+print("[+] Step 4 - Setting Up Virtual Hosts (Recommended)")
+print("[+] Assign ownership of the directory:")
 os.system("sudo chown -R $USER:$USER /var/www/")
-print "[+] The permissions of your web"
+print("[+] The permissions of your web")
 os.system("sudo chmod -R 755 /var/www/")
-print "[+] Disable the default site defined in 000-default.conf:"
+print("[+] Disable the default site defined in 000-default.conf:")
 os.system("sudo a2dissite 000-default.conf")
-print "[+] Test for configuration errors: "
+print("[+] Test for configuration errors: ")
 os.system("sudo apache2ctl configtest ")
-print "[+] Restart Apache to implement your changes:"
+print("[+] Restart Apache to implement your changes:")
 os.system("sudo systemctl reload apache2 ")
 #-----------------------------------------------------------#
-print "-------------------------------------------------------"
+print("-------------------------------------------------------")
 
-print "Step 5: Install MySQL"
+print("Step 5: Install MySQL")
 os.system("sudo apt-get install mysql-server php-mysql -y")
-print "-------------------------------------------------------"
+print("-------------------------------------------------------")
 
-print "Step 6: Install php"
+print("Step 6: Install php")
 # for dvwa
-#os.system("sudo apt-get install php7.2-curl php7.2-mbstring php7.2-xml -y")
-#os.system("sudo apt-get install php7.3-curl php7.3-mbstring php7.3-xml -y")
-#os.system("sudo apt-get install php php-mysqli php-gd libapache2-mod-php -y")
-print "-------------------------------------------------------"
-print "Step 7: Install python"
+os.system("sudo apt-get install php7.2-curl php7.2-mbstring php7.2-xml -y")
+os.system("sudo apt-get install php7.3-curl php7.3-mbstring php7.3-xml -y")
+os.system("sudo apt-get install php php-mysqli php-gd libapache2-mod-php -y")
+print("-------------------------------------------------------")
+print("Step 7: Install python")
 os.system("sudo apt-get install python -y")
 os.system("sudo apt-get install python-pip -y")
 
@@ -64,7 +61,7 @@ os.system("pip install mysql-connector-python")
 
 #os.system("pip install MySQL-python")
 
-print "-------------------------------------------------------"
+print("-------------------------------------------------------")
 # allow url include for dvwa
 # Read in the file
 with open('/etc/php/'+PHP_V+'/apache2/php.ini', 'r') as file:
@@ -77,11 +74,11 @@ filedata = filedata.replace('allow_url_include = Off', 'allow_url_include = On')
 with open('/etc/php/'+PHP_V+'/apache2/php.ini', 'w') as file:
     file.write(filedata)
 time.sleep(0.5)
-print "Restart Apache2..."
+print("Restart Apache2...")
 os.system("sudo service apache2 restart")
-print "-------------------------------------------------------"
+print("-------------------------------------------------------")
 
-print "[+] Now working with the files "
+print("[+] Now working with the files ")
 # unzip files
 os.system("sudo unzip file.zip")
 time.sleep(0.5)
@@ -114,7 +111,7 @@ os.system("sudo mv sqli-labs /var/www/html/")
 
 os.system("sudo rm -r *.zip  *.gz")
 time.sleep(0.5)
-print "[+] Create a sample index.html"
+print("[+] Create a sample index.html")
 time.sleep(0.5)
 os.system("sudo mv index.html /var/www/html/index.html")
 time.sleep(0.5)
@@ -122,7 +119,7 @@ os.system("sudo mv 873.jpg  c-logo-hd.png  /var/www/html")
 
 
 '''
-print "Step 8: Install the vulnerable servers"
+print("Step 8: Install the vulnerable servers")
 os.system("sudo chmod -R 777 /var/www/html/dvwa/external/phpids/0.6/lib/IDS/tmp/phpids_log.txt")
 os.system("sudo chmod -R 777 /var/www/html/dvwa/hackable/uploads")
 os.system("sudo chmod -R 777 /var/www/html/dvwa/config")
@@ -134,7 +131,7 @@ os.system("sudo chmod 777 /var/www/html/bWAPP/logs/")
 os.system("sudo chown -R www-data:www-data /var/www/html/bWAPP/")
 '''
 
-print "Step 9: Create databases "
+print("Step 9: Create databases ")
 import MySQLdb
 # Simple routine to run a query on a database and print the results:
 def doQuery(conn):
@@ -180,30 +177,30 @@ def doQuery(conn):
     cur.execute("flush privileges;")
     time.sleep(0.5)
     for firstname, lastname in cur.fetchall():
-        print firstname, lastname
-print "Using MySQLdb ... "
+        print(firstname, lastname)
+print("Using MySQLdb ... ")
 myConnection = MySQLdb.connect(host=hostname, user=username, passwd=password, db=database)
 doQuery(myConnection)
 myConnection.close()
-print "Creating databases done ...... "
+print("Creating databases done ...... ")
 
 
 
 os.system("clear")
 f = open('banner.txt', 'r')
 file_contents = f.read()
-print(file_contents)
+print("file_contents")
 f.close()
 def op_list():
-    print "Chose any number to install what you want "
-    print "\033[1;30;40m [+] Enter (1) To install { bWAPP      }"
-    print "\033[1;31;40m [+] Enter (2) To install { xvwa       }"
-    print "\033[1;32;40m [+] Enter (3) To install { dvwa       }"
-    print "\033[1;33;40m [+] Enter (4) To install { mutillidae }"
-    print "\033[1;34;40m [+] Enter (5) To install { hackademic }"
-    print "\033[1;35;40m [+] Enter (6) To install { sqli-labs  }"
-    print "\033[1;37;40m [+] Enter (7) To install { ALL        }"
-    print "\033[0;31;40m [+] Enter (8) Or anything To  { Exit  }"
+    print("Chose any number to install what you want ")
+    print("\033[1;30;40m [+] Enter (1) To install { bWAPP      }")
+    print("\033[1;31;40m [+] Enter (2) To install { xvwa       }")
+    print("\033[1;32;40m [+] Enter (3) To install { dvwa       }")
+    print("\033[1;33;40m [+] Enter (4) To install { mutillidae }")
+    print("\033[1;34;40m [+] Enter (5) To install { hackademic }")
+    print("\033[1;35;40m [+] Enter (6) To install { sqli-labs  }")
+    print("\033[1;37;40m [+] Enter (7) To install { ALL        }")
+    print("\033[0;31;40m [+] Enter (8) Or anything To  { Exit  }")
 
 print("\033[1;36;40m Welcome :) This is Nakerah-lab v1 ")
 x = 1
@@ -213,7 +210,7 @@ while x != 0:
         try:
             x = int(input("Enter Your Order :"))
             if x == 1:
-                print "Wait for install { bWAPP      }"
+                print("Wait for install { bWAPP      }")
                 os.system("sudo cp -r bWAPP /var/www/html/")
                 os.system("sudo chmod 777 /var/www/html/bWAPP/passwords/")
                 os.system("sudo chmod 777 /var/www/html/bWAPP/images/")
@@ -221,38 +218,38 @@ while x != 0:
                 #os.system("sudo mkdir /var/www/html/bWAPP/logs/")
                 os.system("sudo chmod 777 /var/www/html/bWAPP/logs/")
                 os.system("sudo chown -R www-data:www-data /var/www/html/bWAPP/")
-		time.sleep(0.5)
+                time.sleep(0.5)
                 op_list()
             elif x == 2:
-                print "Wait for install { xvwa       }"
+                print("Wait for install { xvwa       }")
                 os.system("sudo cp -r xvwa /var/www/html/")
-		time.sleep(0.5)
+                time.sleep(0.5)
                 op_list()
             elif x == 3:
-                print "Wait for install { dvwa       }"
+                print("Wait for install { dvwa       }")
                 os.system("sudo cp -r dvwa /var/www/html/")
                 os.system("sudo chmod -R 777 /var/www/html/dvwa/external/phpids/0.6/lib/IDS/tmp/phpids_log.txt")
                 os.system("sudo chmod -R 777 /var/www/html/dvwa/hackable/uploads")
                 os.system("sudo chmod -R 777 /var/www/html/dvwa/config")
-		time.sleep(0.5)
+                time.sleep(0.5)
                 op_list()
             elif x == 4:
-                print "Wait for install { mutillidae }"
+                print("Wait for install { mutillidae }")
                 os.system("sudo cp -r mutillidae /var/www/html/")
-		time.sleep(0.5)
+                time.sleep(0.5)
                 op_list()
             elif x == 5:
-                print "Wait for install { hackademic }"
+                print("Wait for install { hackademic }")
                 os.system("sudo cp -r hackademic /var/www/html/")
-		time.sleep(0.5)
+                time.sleep(0.5)
                 op_list()
             elif x == 6:
-                print "Wait for install { sqli-labs  }"
+                print("Wait for install { sqli-labs  }")
                 os.system("sudo cp -r sqli-labs /var/www/html/")
-		time.sleep(0.5)
+                time.sleep(0.5)
                 op_list()
             elif x == 7:
-                print "Wait for install { All        }"
+                print("Wait for install { All        }")
                 os.system("sudo cp -r bWAPP /var/www/html/")
                 os.system("sudo chmod 777 /var/www/html/bWAPP/passwords/")
                 os.system("sudo chmod 777 /var/www/html/bWAPP/images/")
@@ -260,32 +257,33 @@ while x != 0:
                 #os.system("sudo mkdir /var/www/html/bWAPP/logs/")
                 os.system("sudo chmod 777 /var/www/html/bWAPP/logs/")
                 os.system("sudo chown -R www-data:www-data /var/www/html/bWAPP/")
-		time.sleep(0.5)
+                time.sleep(0.5)
                 os.system("sudo cp -r xvwa /var/www/html/")
                 os.system("sudo cp -r dvwa /var/www/html/")
                 os.system("sudo chmod -R 777 /var/www/html/dvwa/external/phpids/0.6/lib/IDS/tmp/phpids_log.txt")
                 os.system("sudo chmod -R 777 /var/www/html/dvwa/hackable/uploads")
                 os.system("sudo chmod -R 777 /var/www/html/dvwa/config")
-		time.sleep(0.5)
+                time.sleep(0.5)
                 os.system("sudo cp -r mutillidae /var/www/html/")
                 os.system("sudo cp -r hackademic /var/www/html/")
                 os.system("sudo cp -r sqli-labs /var/www/html/")
-		time.sleep(0.5)
+                time.sleep(0.5)
                 op_list()
         except:
-            print " You can't see the numbers ? enter something exist  "
+            print(" You can't see the numbers ? enter something exist  ")
     else:
         break
-print " Goodbye Friend !!"
+print(" Goodbye Friend !!")
 os.system("clear")
-print "\033[0;32;40m "
+print("\033[0;32;40m ")
 f = open('banner.txt', 'r')
 file_contents = f.read()
-print(file_contents)
+print("file_contents")
 f.close()
 
-print "--------------------------------------------------------------"
-print "Hello Friend ... "
-print "Now u can using yor lab ... "
-print "Just Open your Browser IP Machine = >>  : "
+print("--------------------------------------------------------------")
+print("Hello Friend ... ")
+print("Now u can using yor lab ... ")
+print("Just Open your Browser IP Machine = >>  : ")
 os.system("IP=$(ifconfig|grep -i inet | cut -d ' ' -f 10 | head -1);echo [--] Http://$IP/index.html")
+
