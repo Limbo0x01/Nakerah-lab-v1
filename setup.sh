ef432fc280956982e9992af6a916fa39abc8edd7
Lembo <41475273+0xL1mb0@users.noreply.github.com>
2020-02-05 18:20:28 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 sudo apt-get install php7.2-curl php7.2-mbstring php7.2-xml -y
 sudo apt-get install php7.3-curl php7.3-mbstring php7.3-xml -y
 sudo apt-get install php php-mysqli php-gd libapache2-mod-php -y
-sudo apt install hhvm
+sudo apt install hhvm -y
 
 export PHP_limbo=$(php --version | grep -i 'PHP'|cut -d ')' -f 1 | head -1 | cut -c 5-10|cut -d '.' -f 1,2)
 
