80b120fa7ae0c1ba6cce92cba440797f709d657b
Lembo <41475273+0xL1mb0@users.noreply.github.com>
2020-08-18 02:13:31 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 
 #!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
 sudo apt-get install php7.2-curl php7.2-mbstring php7.2-xml -y
 sudo apt-get install php7.3-curl php7.3-mbstring php7.3-xml -y
 sudo apt-get install php php-mysqli php-gd libapache2-mod-php -y
